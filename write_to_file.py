96e7d4ddba6cce6eff3dab86682271f925049d00
Sai Hardik <74413544+sai-hardik@users.noreply.github.com>
2022-12-30 21:38:13 +0530

    Update write_to_file.py

--- write_to_file.py
+++ write_to_file.py
@@ -1,9 +1,26 @@
+####################################
+#code 1:
 import os
 
 f="sample.txt"
 if os.path.isfile(f):
     try:
         open(f,"a")
+    
+    except Exception as error:
+        print("Permissions denied")
+
+    else: 
+        f.write("world")
+        f.close()
+        
+#####################################
+#code 2:
+f="sample.txt"
+try:
+        open(f,"a")
+    except FileNotFoundError:
+        print("File not found")
     except Exception as error:
         print("Permissions denied")
 
